09238083ef374b88898d2ab3587e9da78c04cff8
Keiddy García <Keiddygarcia@gmail.com>
2023-09-20 13:02:20 -0500

    rm

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,6 @@
 // Styles
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
-import '@fortawesome/fontawesome-free/css/all.css'
-import { aliases, fa } from 'vuetify/iconsets/fa'
-
 // Vuetify
 import { createVuetify } from "vuetify";
 
