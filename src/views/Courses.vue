c88928c680bcf4ad91a6acec5fde11fef8f2588b
Keiddy García <Keiddygarcia@gmail.com>
2023-09-25 09:12:48 -0500

    Update Courses.vue

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-card height="100%" class="pa-12">
-    <h1 class="pb-10">Escoje tu curso</h1>
-    <div class="d-flex flex-row justify-center align-center courses">
-      <v-card
-        v-for="(i, key) in courses"
-        :key="key"
-        class="mr-6 mb-5"
-        max-width="368"
-        elevation="20"
-        style="border-radius: 50px"
-        @click="handleClick(key)"
-      >
-        <img :src="i" class="w-100 h-100" style="border-radius: 50px" />
-      </v-card>
-    </div>
-  </v-card>
+  <main>
+   
+      <h1 class="pb-10">Escoje tu curso</h1>
+      <div class="d-flex flex-row justify-center align-center courses">
+        <v-card
+          v-for="(i, key) in courses"
+          :key="key"
+          class="mr-6 mb-5"
+          max-width="368"
+          elevation="20"
+          style="border-radius: 50px"
+          @click="handleClick(key)"
+        >
+          <img :src="i" class="w-100 h-100" style="border-radius: 50px" />
+        </v-card>
+      </div>
+  </main>
 </template>
 
 <script>
@@ -46,7 +47,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 @media (max-width: 600px) {
   div > .courses {
     flex-direction: column !important;
@@ -54,15 +55,15 @@ export default {
 }
 h1 {
   font-weight: 900;
-  font-size: 7rem!important;
+  font-size: 7rem !important;
   text-align: center;
-  background: linear-gradient(
-    to left,
-    #ff9900,
-    #3399ff,
-    #33cc33
-  );
+  background: linear-gradient(to left, #ff9900, #3399ff, #33cc33);
   color: transparent !important;
   -webkit-background-clip: text;
+}
+main {
+  background-image: url("../assets/img/fondo2.jpg");
+  background-size: cover;
+  height: 100%;
 }
 </style>
