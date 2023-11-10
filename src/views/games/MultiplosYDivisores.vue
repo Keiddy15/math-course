2222f051e06aa48736daf76463aec930f9f081b0
Keiddy García <Keiddygarcia@gmail.com>
2023-11-09 21:10:57 -0500

    Create MultiplosYDivisores.vue

---
+++ src/views/games/MultiplosYDivisores.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <h1
+      style="
+        text-align: center;
+        font-weight: 900;
+        color: #1d5d9b;
+        font-size: 4rem;
+      "
+      class="mt-4"
+    >
+      Múltiplos y Divisores
+    </h1>
+    <div style="text-align: center">
+      <iframe
+        allow="fullscreen; autoplay;"
+        allowfullscreen
+        width="80%"
+        height="690"
+        frameborder="0"
+        src="https://www.educaplay.com/game/15680493-multiplos_y_divisores.html"
+      ></iframe>
+    </div>
+  </main>
+</template>
+  
+  <script>
+export default {
+  name: "MultiplosYDivisores",
+  data: () => ({
+    game: "",
+  }),
+  created() {},
+};
+</script>
+  
+  <style>
+</style>
